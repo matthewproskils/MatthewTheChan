97558e41037f27e80191bef77977ad44b2773505
matthewproskils <65041704+matthewproskils@users.noreply.github.com>
2021-05-08 19:42:18 -0500

    Create Matthew Chan.js

---
+++ raw/Matthew Chan.js
@@ -0,0 +1,15 @@
+let BirthStats = {
+    Birthplace: "Ca, USA",
+    Birthday: "June 27",
+    Age: "12"
+};
+
+/*
+* Programmer Hobbyist
+* Youtube Lover
+* And not really much of anything else
+*/
+
+let DiscordId = "Mr;5320";
+let GithubUsername = "MatthewTheChan";
+let ReplitUsername = "MatthewProSkils";
